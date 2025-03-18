7b5221375ba1ddcdf9ca2775566b30fddbd563a7
Sisi Wang <sisiwang242@gmail.com>
2025-03-18 19:32:19 -0400

    Initial Commit

---
+++ Pre-work01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pre-work01
+//
+//  Created by Sisi Wang on 3/18/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pre-work01Tests/Pre_work01Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Pre_work01Tests.swift
+//  Pre-work01Tests
+//
+//  Created by Sisi Wang on 3/18/25.
+//
+
+import Testing
+@testable import Pre_work01
+
+struct Pre_work01Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
